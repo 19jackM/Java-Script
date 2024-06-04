751f0d0a3302ba9fafe7b9695f5e88ca49d81af1
19jackM <jack.mokoena1234@gmail.com>
2024-06-04 09:43:40 +0200

    newcode

--- main.js
+++ main.js
@@ -1,15 +1,58 @@
+// let Fname = prompt('enter your name');
+// let Age = Number(prompt('Enter your age'));
+// let citizen = prompt('Enter your citizenship');
+
+
+// if(Age >= 18 && citizen === 'SA'){
+// alert('Hi ' + Fname+ ' Congres you qualifiy to vote!!');
+// } else{
+
+//     alert('Hi ' +  " Sorry you don't qualifiy to vote! \n Coz you either young or not SA citizen");
+// }
+
+
+// let num1 = Number(prompt('enter nummber'));
+// op = prompt('Enter your operator');
+// let numb2 = Number(prompt('Enter second number'))
+
+
+
+// if (op==='+'){
+
+// console.log('your answer is ' +${num1+numb2} );
+
+// } else if (op ==='-'){
+//     console.log(num1- numb2);
+// } else if(op ==='')
+
+// {
+
+//     console.log(num1 numb2);
+// }
+
+// else if (op ==='/')
+
+// {
+
+//     console.log(num1/numb2);
+// }
+
+// else
+// {
+//     console.log('invalid operator')
+// }
+
+
 let Fname = prompt('enter your name');
-let Age = Number(prompt('Enter your age'));
+
+
+let username = prompt('Admin');
+let password (prompt('password'));
 let citizen = prompt('Enter your citizenship');
 
 
 if(Age >= 18 && citizen === 'SA'){
 alert('Hi ' + Fname+ ' Congres you qualifiy to vote!!');
-} else{
-
-    alert('Hi ' +  " Sorry you don't qualifiy to vote! \n Coz you either young or not SA citizen");
-}
-
-
+} else
 
 
